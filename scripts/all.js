ff2be7bd96b1e60258daede6f5a051dee3873ed8
Mena Wang <wang.mena@gmail.com>
2021-08-02 23:37:48 -0700

    wooooo pineapples

--- scripts/all.js
+++ scripts/all.js
@@ -1,15 +1,60 @@
-/* smooth scroll */
-$('.nav').localScroll({
-  duration: 400,
-  easing: 'swing'
+/////////////   MAIN FUNCTIONS  ////////////////
+
+//make another pineapple appear on click
+function click(evt) {
+  // var c = document.getElementById("mainCanvas");
+  // c.width = window.innerWidth; // give canvas same width...
+  // c.height = window.innerHeight; // and height as our window.
+
+  var div = document.createElement("div");
+  div.classList.add("pineapple");
+
+  //pineapple image
+  var pineapple = document.createElement("img");
+  pineapple.src = "images/pineapple" + Math.floor(Math.random() * 3) + ".svg";
+
+  var original_width = pineapple.width + 1;
+  var original_height = pineapple.height + 1;
+
+  pineapple.width = Math.random() * 200 + 50;
+  pineapple.height = (pineapple.width * original_width) / original_height;
+
+  div.style.position = "fixed";
+  div.style.marginLeft = Math.random() * 1000 + pineapple.width + "px";
+  div.style.marginTop = Math.random() * 500 - pineapple.height + "px";
+
+  var degrees = Math.random() * 180;
+
+  div.style.transform = "rotate(".concat(degrees.toString(), "deg)");
+
+  div.appendChild(pineapple);
+
+  document.body.appendChild(div);
+}
+
+// function clear(evt) {
+//   var allDogs = document.getElementsByClassName("dogdiv");
+//   console.log(allDogs);
+//   for (var i = 0; i < allDogs.length; i++) {
+//     allDogs.item(i).style.display = "none";
+//   }
+// }
+
+/////////////   SETUP THE HANDLERS  ////////////////
+var inter;
+var numPineapples = 0;
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log(event);
+  inter = setInterval(function () {
+    numPineapples += 1;
+    if (numPineapples < 50) {
+      click(event);
+    } else {
+      document.body.innerHTML = "";
+      numPineapples = 0;
+    }
+  }, 1000);
 });
-
-$('.name').localScroll({
-  duration: 400,
-  easing: 'swing'
-})
-
-$('.projects_link').localScroll({
-  duration: 400,
-  easing: 'swing'
-})
+//document.addEventListener("mousemove", tongueAngle);
+//document.addEventListener("mousedown", click);
+//document.getElementById("button").addEventListener("mousedown", clear);
